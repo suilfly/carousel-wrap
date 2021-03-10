c5c61b0813123d59137a0fa400cce5b48c287763
suilfly <876794103@qq.com>
2021-03-10 20:55:45 +0800

    实现无限轮播效果

--- js/carousel-wrap.js
+++ js/carousel-wrap.js
@@ -1,11 +1,11 @@
 class Carousel {
     /* 定义Carousel的私有属性 */
-    constructor(imgSrc, wrapElement) {
+    constructor(imgSrc, carouselWrap) {
         this.imgSrc = imgSrc || ["/img/1.jpg",
             "/img/2.jpg",
             "/img/3.jpg"];
-        this.wrapElement = wrapElement;
-        this.init(imgSrc, wrapElement);
+        this.carouselWrap = carouselWrap;
+        this.init(imgSrc, carouselWrap);
     }
 }
 (function(){
@@ -13,11 +13,14 @@ Carousel.prototype.init = carousel;
 /* 使用插件的用户的存放图片path的数组不一定名：src，所以通过一个函数来规定 */
 function createLiDom(src,carouselWrap) {
 
+    /* 增加src形成视觉循环 */
+    src.splice(0,0,src[src.length-1]);
+    src.splice(src.length,0,src[1]);console.log(src)
     let ul = document.createElement("ul");
     let ulWidth = src.length * 100;/* 动态计算ul的宽度 */
     let liWidth = 100 / src.length;/* 动态计算ul>li的宽度 */
     let liFromUl = ul.getElementsByTagName('li');
-
+ 
 
     ul.classList.add('my-list');
     /* 动态创建DOM节点 */
@@ -39,39 +42,51 @@ function createLiDom(src,carouselWrap) {
         carouselWrap.style.height = img.clientHeight + 'px';
         clearInterval(imgTime);
     }, 500);
-    return {ul,liFromUl};
+    return {ul,liFromUl,src};
 }
-function touchMove(carouselWrap,{ul,liFromUl}){
+function touchMove(_this,{ul,liFromUl,src}){
+        let viewWidth = document.documentElement.clientWidth;
         /* 滑屏相关 */
         let touchStartX = 0, touchdis = 0;
         /* 滑动的元素的起始位置 */
-        let ulX = 0;
+        let ulX = -viewWidth;
         /* 添加移动端事件 */
-
-        carouselWrap.addEventListener('touchstart', function (e) {
+        ul.style.left = ulX+'px';
+        _this.carouselWrap.addEventListener('touchstart', function (e) {
             let touch = e.changedTouches[0];/* TouchList 接口代表一个触摸平面上所有触点的列表。例如，如果一个用户用三根手指接触屏幕（或者触控板），与之对应的 TouchList 会包含每根手指的 Touch 对象，总共三个 */
+            let absOffLeft = Math.abs(ul.offsetLeft);
+            
             touchStartX = touch.clientX;
             ulX = ul.offsetLeft;
             ul.style.transition = "";
+     
+           
+            
         });
-        carouselWrap.addEventListener('touchmove', function (e) {
+        _this.carouselWrap.addEventListener('touchmove', function (e) {
             let touch = e.changedTouches[0];
+            /* let absOffLeft = Math.abs(ul.offsetLeft);
+            let viewWidth = document.documentElement.clientWidth; */
             touchdis = touch.clientX - touchStartX;
             ul.style.left = ulX + touchdis + 'px';
+           /*  if(absOffLeft/viewWidth == liFromUl.length-1){ console.log(111); } */
         });
-        carouselWrap.addEventListener('touchend', function (e) {
+        _this.carouselWrap.addEventListener('touchend', function (e) {
             let index = ul.offsetLeft / document.documentElement.clientWidth;/* 用ul的实时位置抽象成下标，若index的下标为负：向左滑，为正：向右滑 */
             index = Math.round(index)
 
             /* 设置超出部分 */
 
             if (Math.abs(touchdis) >= document.documentElement.clientWidth / 2) { /* 移动距离大于视口的一半 */
-                if (index > 0) {
-                    index = 0;
-                } if (Math.abs(index) >= liFromUl.length - 1) {
-                    index = -(liFromUl.length - 1);
+                if (index > -1) {
+                    index = -(src.length-2);/* 实际上的图片第一张是最后一张，最后一张是第一张 */
+                } else if (Math.abs(index) == liFromUl.length - 1) {/* 当滑到理论上最后一张（实际倒数第二张） */
+                    index = -1;
                 }
-                ul.style.transition = ".5s left";
+                else{
+                    ul.style.transition = " left .5s";
+                }
+                
                 ul.style.left = index * (document.documentElement.clientWidth) + 'px'; console.log(index);/* 每次移动一个视口的宽度  Math.round(index):四舍五入返回整数*/
             }
             else {
@@ -81,13 +96,25 @@ function touchMove(carouselWrap,{ul,liFromUl}){
 
         });
 }
+/* 自动轮播 */
+function autoPlay({ul,liFromUl}){
+let stepLengh = ul.querySelector("li img").clientWidth;
+ul.style.transition = "left 1.5s";
+ul.style.left = ul.offsetLeft+'px';
+setInterval(()=>{
+    let index = Math.round(ul.offsetLeft/document.documentElement.clientWidth);
+    ul.style.left =(--index) * (document.documentElement.clientWidth) + 'px';
+},5000)
+}
+
 function carousel(src = ["/img/1.jpg",
     "/img/2.jpg",
     "/img/3.jpg"], carouselWrap) {
     let ul;
     if (carouselWrap) {
         ul=createLiDom(src,carouselWrap);
-        touchMove(carouselWrap,ul);
+        /* autoPlay(ul); */
+        touchMove(this,ul);
     }
 
 }
